1409f034869d88a065f9349f101842a9d39dff93
aashi0711 <dharawatayushi@gmail.com>
2018-01-15 16:33:18 +0530

    FreshMaal

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SideBar from './sidebar/sidebar';
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <SideBar/>
+        </div>
+    );
+  }
+}
+
+export default App;
